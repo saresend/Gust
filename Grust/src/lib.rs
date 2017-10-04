8a5bc3b8e3fcfabbe551b4fd99345cb1d5f7c597
Samuel Resendez <saresend@usc.edu>
2017-10-04 11:03:01 -0700

    editing file_start tests

--- Grust/src/lib.rs
+++ Grust/src/lib.rs
@@ -8,13 +8,14 @@ mod render_d3;
 
 #[cfg(test)]
 mod tests {
-
+    use std::fs::remove_file;
     use render_d3::start_file;
     #[test]
     fn test_start_file() {
         match start_file("test.html".to_string()) {
-            Ok(_) => {},
+            Ok(_) => println!("{:?}", remove_file("text.html")),
             Err(_) => panic!("Failed to create file"),
         }
+        
     }
 }
