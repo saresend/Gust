8a11d60e86e35e39dea1c3a398e1763ba8aea13b
Samuel Resendez <saresend@usc.edu>
2017-10-04 23:25:05 -0700

    refactored to begin support serialization

--- Grust/src/axes.rs
+++ Grust/src/axes.rs
@@ -1,13 +1,13 @@
+extern crate serde_json;
 
-
-
+#[derive(Serialize, Deserialize)]
 pub struct XAxis {
     title: String,
     unit: String,
     has_line: bool,
     max_value: i64,
 }
-
+#[derive(Serialize, Deserialize)]
 pub struct YAxis {
     title: String,
     unit: String,
@@ -18,9 +18,30 @@ pub struct YAxis {
 
 
 impl XAxis {
+    pub fn to_json(&self) -> String {
+        match serde_json::to_string(&self) {
+            Ok(s) => s,
+            Err(_) => "".to_string(),
+        }
 
-    
+    }
 
 
+    pub fn new(title: String, unit: String, has_line: bool, max_value: i64) -> XAxis {
+        XAxis {
+            title,
+            unit,
+            has_line,
+            max_value,
+        }
+    }
+}
 
+impl YAxis {
+    pub fn to_json(&self) -> String {
+        match serde_json::to_string(&self) {
+            Ok(s) => s,
+            Err(_) => "".to_string(),
+        }
+    }
 }
--- Grust/src/lib.rs
+++ Grust/src/lib.rs
@@ -1,21 +1,24 @@
 
+extern crate serde;
+extern crate serde_json;
+
+#[macro_use]
+extern crate serde_derive;
 
 mod bar_chart;
 mod axes;
 mod bar;
-mod render_d3;
 
 
 #[cfg(test)]
 mod tests {
     use std::fs::remove_file;
-    use render_d3::start_file;
+    use axes::{YAxis, XAxis};
     #[test]
-    fn test_start_file() {
-        match start_file("test.html".to_string()) {
-            Ok(_) => println!("{:?}", remove_file("text.html")),
-            Err(_) => panic!("Failed to create file"),
-        }
-        
+    fn test_XAxis_Serialization() {
+        let x1 = XAxis::new("Hi there".to_string(), "Feet".to_string(), false, 200);
+
+        println!("{}", x1.to_json());
     }
+    
 }
