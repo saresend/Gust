bd3a1caa1e45f461b0b9c581d22cc207fc3e11ea
Samuel Resendez <saresend@usc.edu>
2017-12-08 12:47:56 -0800

    added top-level graph

--- src/backend/graph.rs
+++ src/backend/graph.rs
@@ -3,20 +3,65 @@
  */
 
 use serde::ser::Serialize;
+use serde::Serializer;
+use serde::ser::SerializeStruct;
 
 pub struct Graph {
-    schema: String,
-    description: String,
+    schema:&'static str,
+    pub description: String,
+    pub width: u16,
+    pub height: u16,
+    pub padding: u16,
+    pub autosize: AutoSize,
+
+    pub signals: Vec<Signal>,
 }
 
+/* 
+ * AutoSize represents the different padding options that exist in Vega
+ */
+#[derive(Serialize)]
+#[serde(rename_all="lowercase")]
+pub enum AutoSize {
+    Pad,
+    Fit, 
+    None,
+}
+
+
+impl Graph {
+
+    //TODO: As Graph module gets fleshed out add more
+    pub fn new(description: String) -> Graph {
+        Graph {
+            schema: "https://vega.github.io/schema/vega/v3.0.json",
+            description: description,
+            width: 500,
+            height: 200,
+            padding: 5,
+            autosize: AutoSize::None,
+        }
+    }
+}
+
+
+/*
+ * -------------------------------------------------------------
+ * This section begins Trait implementations for the Graph Struct 
+ * -------------------------------------------------------------
+ */
 impl Serialize for Graph {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
     {
         let mut s = serializer.serialize_struct("Graph", 2)?;
-        s.serialize_field("$schema", &self.schema);
-        s.serialize_field("description", &self.description);
+        let _ = s.serialize_field("$schema", &self.schema);
+        let _ = s.serialize_field("description", &self.description);
+        let _ = s.serialize_field("width", &self.width);
+        let _ = s.serialize_field("height", &self.height);
+        let _ = s.serialize_field("padding", &self.padding);
+        let _ = s.serialize_field("autosize", &self.autosize);
         s.end()
     }
 }
--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -1,10 +1,11 @@
 
 pub mod graph;
 pub mod signal;
+/*
 pub mod data;
 pub mod scale;
 pub mod projection;
 pub mod axis;
 pub mod legend;
-pub mod mark;
+pub mod mark; */
 pub mod io;
