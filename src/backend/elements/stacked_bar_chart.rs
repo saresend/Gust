9c4cc7d4bd538645859653035df52c778025a203
Samuel Resendez <saresend@usc.edu>
2017-12-17 18:35:23 -0800

    implementing StackedBarElements

--- src/backend/elements/stacked_bar_chart.rs
+++ src/backend/elements/stacked_bar_chart.rs
@@ -1,7 +1,7 @@
 
 
 use backend::elements::general::*;
-
+use serde::ser::{Serialize, SerializeStruct, Serializer};
 
 #[derive(Serialize)]
 pub struct StackedBarData {
@@ -23,6 +23,9 @@ impl StackedBarData {
             values: vec![],
         }
     }
+    pub fn add_data(&mut self, x: i64, y: i64, z: i64) {
+        self.values.push(StackedBarDataValue { x, y, z });
+    }
 }
 
 pub struct StackedBarTransform {
@@ -31,8 +34,55 @@ pub struct StackedBarTransform {
     pub sort: KeyVal,
     pub field: String,
 }
+impl StackedBarTransform {
+    pub fn new() -> StackedBarTransform {
+        StackedBarTransform {
+            transform_type: String::from("stack"),
+            group_by: vec![String::from("x")],
+            sort: KeyVal::new("field", "c"),
+            field: String::from("y"),
+        }
+    }
+}
+impl Serialize for StackedBarTransform {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut s = serializer.serialize_struct("transform", 3)?;
+        s.serialize_field("type", &self.transform_type)?;
+        s.serialize_field("groupby", &self.group_by)?;
+        s.serialize_field("sort", &self.sort)?;
+        s.serialize_field("field", &self.field)?;
+        s.end()
+    }
+}
 
-pub struct StackedBarScale {}
+pub struct StackedBarScale {
+    name: String,
+    scale_type: String,
+    range: String,
+    domain: JSONDict,
+}
+impl StackedBarScale {
+    pub fn new_xscale() -> StackedBarScale {
+        StackedBarScale {
+            name: String::from("x"),
+            scale_type: String::from("band"),
+            range: String::from("width"),
+            domain: JSONDict::create("data", "table", "field", "x"),
+        }
+    }
+    pub fn new_yscale() -> StackedBarScale {
+        StackedBarScale {
+            name: String::from("y"),
+            scale_type: String::from("linear"),
+            range: String::from("height"),
+            domain: JSONDict::create("data", "table", "field", "y1"),
+        }
+    }
+    pub fn new_ordinal_scale() -> StackedBarScale {}
+}
 
 pub struct StackedBarAxis {}
 
--- src/backend/stacked_bar_chart.rs
+++ src/backend/stacked_bar_chart.rs
@@ -10,11 +10,34 @@ pub struct StackedBarChart {
     pub identifier: String,
     pub width: u16,
     pub height: u16,
-    pub padding: f32,
+    pub padding: u16,
 
     pub data: Vec<StackedBarData>,
     pub transform: Vec<StackedBarTransform>,
     pub scales: Vec<StackedBarScale>,
     pub axes: Vec<StackedBarAxis>,
     pub marks: Vec<StackedBarMark>,
+}
+
+
+impl StackedBarChart {
+    pub fn new() -> StackedBarChart {
+        StackedBarChart {
+            identifier: String::from("stacked_bar_chart"),
+            width: 500,
+            height: 300,
+            padding: 5,
+            data: vec![],
+
+            transform: vec![StackedBarTransform::new()],
+
+            scales: vec![
+                StackedBarScale::new_xscale(),
+                StackedBarScale::new_yscale(),
+                StackedBarScale::new_ordinal_scale(),
+            ],
+            axes: vec![StackedBarAxis::new_xaxis(), StackedBarAxis::new_yaxis()],
+            marks: vec![StackedBarMark::new()],
+        }
+    }
 }
