08bab7c643f1f875b5781223ceb0332601501da0
Samuel Resendez <saresend@usc.edu>
2017-12-17 21:41:57 -0800

    added most of scale implementations

--- src/backend/elements/line_chart.rs
+++ src/backend/elements/line_chart.rs
@@ -1,5 +1,8 @@
 
 
+
+use backend::elements::general::*;
+
 #[derive(Serialize)]
 pub struct LineChartSignal {
     pub name: String,
@@ -40,9 +43,63 @@ impl LineChartInterpolation {
     }
 }
 
-pub struct LineChartData {}
+#[derive(Serialize)]
+pub struct LineChartData {
+    pub name: String,
+    pub values: Vec<LineChartDataValue>,
+}
 
-pub struct LineChartScale {}
+impl LineChartData {
+    pub fn new() -> LineChartData {
+        LineChartData {
+            name: String::from("table"),
+            values: vec![],
+        }
+    }
+    pub fn add_data(&mut self, x: i64, y: i64, z: i64) {
+        self.values.push(LineChartDataValue { x, y, z });
+    }
+}
+
+#[derive(Serialize)]
+pub struct LineChartDataValue {
+    x: i64,
+    y: i64,
+    z: i64,
+}
+
+pub struct LineChartScale {
+    pub name: String,
+    pub scale_type: String,
+    pub range: String,
+    pub domain: JSONDict,
+}
+impl LineChartScale {
+    pub fn new_xscale() -> LineChartScale {
+        LineChartScale {
+            name: String::from("x"),
+            scale_type: String::from("point"),
+            range: String::from("width"),
+            domain: JSONDict::create("data", "table", "field", "x"),
+        }
+    }
+    pub fn new_yscale() -> LineChartScale {
+        LineChartScale {
+            name: String::from("y"),
+            scale_type: String::from("linear"),
+            range: String::from("height"),
+            domain: JSONDict::create("data", "table", "field", "y"),
+        }
+    }
+    pub fn new_ordinal_scale() -> LineChartScale {
+        LineChartScale {
+            name: String::from("color"),
+            scale_type: String::from("ordinal"),
+            range: String::from("category"),
+            domain: JSONDict::create("data", "table", "field", "z"),
+        }
+    }
+}
 
 pub struct LineChartAxis {}
 
--- src/backend/line_chart.rs
+++ src/backend/line_chart.rs
@@ -13,3 +13,23 @@ pub struct LineChart {
     pub axes: Vec<LineChartAxis>,
     pub marks: Vec<LineChartMark>,
 }
+
+impl LineChart {
+    pub fn new() -> LineChart {
+        LineChart {
+            identifier: String::from("line_chart"),
+            width: 500,
+            height: 300,
+            padding: 5,
+            signals: vec![],
+            data: vec![],
+            scales: vec![
+                LineChartScale::new_xscale(),
+                LineChartScale::new_yscale(),
+                LineChartScale::new_ordinal_scale(),
+            ],
+            axes: vec![],
+            marks: vec![],
+        }
+    }
+}
