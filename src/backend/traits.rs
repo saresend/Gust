566058a5670f63e31a7e1eda197d3a70a1e4b3e6
Samuel Resendez <saresend@usc.edu>
2018-02-02 11:03:33 -0800

    added required trait functions for Graphable

--- src/backend/traits.rs
+++ src/backend/traits.rs
@@ -10,4 +10,10 @@ pub trait Graphable: Sized + Serialize {
 
     fn get_description(&self) -> &str;
     fn get_identifier(&self) -> &str;
+
+    // Define the API for what we can adjust through a Graph
+    // set_color will certainly be part of the API, likely accepting a &str
+    fn set_color(&str);
+    // set_dimensions
+    fn set_dimensions((u32, u32));
 }
