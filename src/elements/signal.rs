153f357886ccd73748a82505884229834a25a763
saresend <saresend@usc.edu>
2019-01-14 17:46:07 -0800

    now implements signal

--- src/elements/signal.rs
+++ src/elements/signal.rs
@@ -1,4 +1,5 @@
 use super::binding::Binding;
+use serde::ser::{Serialize, SerializeStruct, Serializer};
 
 #[derive(Serialize, Deserialize)]
 pub enum JSAny {
@@ -10,13 +11,40 @@ pub enum JSAny {
 }
 
 #[derive(Serialize, Deserialize)]
+pub enum VegaEvaluation {
+    #[serde(rename = "init")]
+    Init(Expression),
+    #[serde(rename = "on")]
+    On(Expression),
+}
+#[derive(Deserialize)]
 pub struct Signal {
     name: String,
     bind: Binding,
     description: String,
-    on: Vec<Handler>,
-    init: Option<String>,
+    eval: VegaEvaluation,
     update: Option<String>,
     react: bool,
-    value: String,
+    value: JSAny,
+}
+
+// Needed so that we can enforce mutual exclusion of init and on at compile time
+impl Serialize for Signal {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut signal = serializer.serialize_struct("Signal", 7)?;
+        signal.serialize_field("name", &self.name)?;
+        signal.serialize_field("bind", &self.bind)?;
+        signal.serialize_field("description", &self.bind)?;
+        match self.eval {
+            VegaEvaluation::Init(ref exp) => signal.serialize_field("init", exp)?,
+            VegaEvaluation::On(ref exp) => signal.serialize_field("on", exp)?,
+        };
+        signal.serialize_field("update", &self.update)?;
+        signal.serialize_field("react", &self.react)?;
+        signal.serialize_field("value", &self.value)?;
+        signal.end()
+    }
 }
