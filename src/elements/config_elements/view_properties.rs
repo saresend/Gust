939385396516eaa94ff694373a8ecc385ce95f62
saresend <saresend@usc.edu>
2019-01-17 17:23:08 -0800

    viewProperties finished

--- src/elements/config_elements/view_properties.rs
+++ src/elements/config_elements/view_properties.rs
@@ -1,15 +1,31 @@
 use super::super::autosize::Autosize;
 
-#[derive(Serialize)]
+use serde::ser::{Serialize, SerializeStruct, Serializer};
+
 pub enum GroupOptions {
-    #[serde(rename = "fill")]
     Fill(String),
-    #[serde(rename = "stroke")]
     Stroke(String),
-    #[serde(rename = "strokeWidth")]
     StrokeWidth(i32),
 }
 
+impl Serialize for GroupOptions {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut group_options = serializer.serialize_struct("GroupOptions", 1)?;
+        match &self {
+            GroupOptions::Fill(color) => group_options.serialize_field("fill", color)?,
+            GroupOptions::Stroke(color) => group_options.serialize_field("stroke", color)?,
+            GroupOptions::StrokeWidth(color) => {
+                group_options.serialize_field("strokeWidth", color)?
+            }
+        }
+        group_options.end()
+    }
+}
+
+#[derive(Serialize)]
 pub struct ViewProperties {
     autosize: Autosize,
     background: String,
